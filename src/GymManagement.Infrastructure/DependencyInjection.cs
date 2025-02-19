f29a5adbfe1f47870cf2b9f2ccea41b9db3f7e3b
andrei <atincu@jigsawcreate.com>
2025-02-19 15:15:51 +0200

    add migrations and create DB on startup

--- src/GymManagement.Infrastructure/DependencyInjection.cs
+++ src/GymManagement.Infrastructure/DependencyInjection.cs
@@ -14,6 +14,14 @@ public static class DependencyInjection
       options.UseSqlite("Data Source=gym_management.db");
     });
 
+    // Create a scope to resolve the DbContext and apply migrations
+    using (var serviceProvider = services.BuildServiceProvider())
+    {
+      using var scope = serviceProvider.CreateScope();
+      var dbContext = scope.ServiceProvider.GetRequiredService<GymManagementDbContext>();
+      dbContext.Database.Migrate();
+    }
+
     services.AddScoped<IUnitOfWork>(provider => provider.GetRequiredService<GymManagementDbContext>());
     services.AddScoped<ISubscriptionsRepository, SubscriptionRepository>();
     return services;
